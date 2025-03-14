8ef1f1a4d8b8705a88bd0cfca90587c67dc288d1
zaDO <43agsr@gmail.com>
2025-03-14 16:46:14 +0530

    Minor changes to the UI.

--- FastFoodRestaurantSystem/DashboardForm.Designer.vb
+++ FastFoodRestaurantSystem/DashboardForm.Designer.vb
@@ -27,23 +27,26 @@ Partial Class DashboardForm
         Me.btnIneventory = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(267, 25)
+        Me.Label1.Font = New System.Drawing.Font("Consolas", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(315, 48)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(211, 13)
+        Me.Label1.Size = New System.Drawing.Size(149, 32)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Fast Food Restaurant Management System"
+        Me.Label1.Text = "Dashboard"
         '
         'btnOrder
         '
         Me.btnOrder.AccessibleName = ""
-        Me.btnOrder.Location = New System.Drawing.Point(41, 119)
+        Me.btnOrder.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOrder.Location = New System.Drawing.Point(94, 171)
         Me.btnOrder.Name = "btnOrder"
-        Me.btnOrder.Size = New System.Drawing.Size(75, 23)
+        Me.btnOrder.Size = New System.Drawing.Size(92, 23)
         Me.btnOrder.TabIndex = 1
         Me.btnOrder.Text = "Order"
         Me.btnOrder.UseVisualStyleBackColor = True
@@ -51,9 +54,10 @@ Partial Class DashboardForm
         'btnIneventory
         '
         Me.btnIneventory.AccessibleName = ""
-        Me.btnIneventory.Location = New System.Drawing.Point(41, 178)
+        Me.btnIneventory.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIneventory.Location = New System.Drawing.Point(94, 209)
         Me.btnIneventory.Name = "btnIneventory"
-        Me.btnIneventory.Size = New System.Drawing.Size(75, 23)
+        Me.btnIneventory.Size = New System.Drawing.Size(92, 23)
         Me.btnIneventory.TabIndex = 2
         Me.btnIneventory.Text = "Inventory"
         Me.btnIneventory.UseVisualStyleBackColor = True
@@ -61,18 +65,32 @@ Partial Class DashboardForm
         'btnExit
         '
         Me.btnExit.AccessibleName = ""
+        Me.btnExit.BackColor = System.Drawing.Color.IndianRed
+        Me.btnExit.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.White
         Me.btnExit.Location = New System.Drawing.Point(697, 404)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(75, 23)
         Me.btnExit.TabIndex = 3
         Me.btnExit.Text = "Exit"
-        Me.btnExit.UseVisualStyleBackColor = True
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Copperplate Gothic Light", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(337, 31)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(101, 17)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "BuddyFast"
         '
         'DashboardForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnIneventory)
         Me.Controls.Add(Me.btnOrder)
@@ -89,4 +107,5 @@ Partial Class DashboardForm
     Friend WithEvents btnIneventory As Button
     Friend WithEvents btnExit As Button
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label5 As Label
 End Class
--- FastFoodRestaurantSystem/OrderForm.Designer.vb
+++ FastFoodRestaurantSystem/OrderForm.Designer.vb
@@ -28,46 +28,55 @@ Partial Class OrderForm
         Me.lstOrderDetails = New System.Windows.Forms.ListBox()
         Me.lblTotal = New System.Windows.Forms.Label()
         Me.btnCheckout = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'cmbFoodItems
         '
         Me.cmbFoodItems.FormattingEnabled = True
-        Me.cmbFoodItems.Location = New System.Drawing.Point(199, 62)
+        Me.cmbFoodItems.Location = New System.Drawing.Point(211, 159)
         Me.cmbFoodItems.Name = "cmbFoodItems"
         Me.cmbFoodItems.Size = New System.Drawing.Size(121, 21)
         Me.cmbFoodItems.TabIndex = 0
         '
         'txtQuantity
         '
-        Me.txtQuantity.Location = New System.Drawing.Point(353, 62)
+        Me.txtQuantity.Location = New System.Drawing.Point(211, 198)
         Me.txtQuantity.Name = "txtQuantity"
-        Me.txtQuantity.Size = New System.Drawing.Size(100, 20)
+        Me.txtQuantity.Size = New System.Drawing.Size(121, 20)
         Me.txtQuantity.TabIndex = 1
         '
         'btnAddToCart
         '
-        Me.btnAddToCart.Location = New System.Drawing.Point(510, 62)
+        Me.btnAddToCart.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAddToCart.Location = New System.Drawing.Point(222, 241)
         Me.btnAddToCart.Name = "btnAddToCart"
-        Me.btnAddToCart.Size = New System.Drawing.Size(75, 23)
+        Me.btnAddToCart.Size = New System.Drawing.Size(100, 23)
         Me.btnAddToCart.TabIndex = 2
         Me.btnAddToCart.Text = "Add to cart"
         Me.btnAddToCart.UseVisualStyleBackColor = True
         '
         'lstOrderDetails
         '
+        Me.lstOrderDetails.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lstOrderDetails.FormattingEnabled = True
-        Me.lstOrderDetails.Location = New System.Drawing.Point(657, 62)
+        Me.lstOrderDetails.ItemHeight = 15
+        Me.lstOrderDetails.Location = New System.Drawing.Point(469, 146)
         Me.lstOrderDetails.Name = "lstOrderDetails"
-        Me.lstOrderDetails.Size = New System.Drawing.Size(120, 95)
+        Me.lstOrderDetails.Size = New System.Drawing.Size(319, 184)
         Me.lstOrderDetails.TabIndex = 3
         '
         'lblTotal
         '
         Me.lblTotal.AutoSize = True
-        Me.lblTotal.Location = New System.Drawing.Point(703, 364)
+        Me.lblTotal.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(612, 333)
         Me.lblTotal.Name = "lblTotal"
-        Me.lblTotal.Size = New System.Drawing.Size(31, 13)
+        Me.lblTotal.Size = New System.Drawing.Size(42, 15)
         Me.lblTotal.TabIndex = 4
         Me.lblTotal.Text = "Total"
         '
@@ -75,18 +84,73 @@ Partial Class OrderForm
         '
         Me.btnCheckout.BackColor = System.Drawing.Color.IndianRed
         Me.btnCheckout.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.btnCheckout.Location = New System.Drawing.Point(681, 403)
+        Me.btnCheckout.Location = New System.Drawing.Point(594, 372)
         Me.btnCheckout.Name = "btnCheckout"
         Me.btnCheckout.Size = New System.Drawing.Size(75, 23)
         Me.btnCheckout.TabIndex = 5
         Me.btnCheckout.Text = "Checkout"
         Me.btnCheckout.UseVisualStyleBackColor = False
         '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Copperplate Gothic Light", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(337, 27)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(101, 17)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "BuddyFast"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Consolas", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(258, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(254, 32)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Ordering Section"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(86, 159)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(119, 15)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Select an item :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(128, 198)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 15)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Quantity :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(612, 128)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 15)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Cart"
+        '
         'OrderForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnCheckout)
         Me.Controls.Add(Me.lblTotal)
         Me.Controls.Add(Me.lstOrderDetails)
@@ -106,4 +170,9 @@ Partial Class OrderForm
     Friend WithEvents lstOrderDetails As ListBox
     Friend WithEvents lblTotal As Label
     Friend WithEvents btnCheckout As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
