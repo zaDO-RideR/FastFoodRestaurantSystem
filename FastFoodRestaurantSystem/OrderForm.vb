757dc3ebbd09c32cee748e215528580f06650bf3
zaDO <43agsr@gmail.com>
2025-03-14 10:42:44 +0530

    UI for Order Form added.

---
+++ FastFoodRestaurantSystem/OrderForm.vb
@@ -0,0 +1,9 @@
+﻿Public Class OrderForm
+    Private Sub OrderForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbFoodItems.Items.Add("Burgar - 700LKR")
+        cmbFoodItems.Items.Add("Pizza - 2000LKR")
+        cmbFoodItems.Items.Add("Fries - 400LKR")
+        cmbFoodItems.Items.Add("Coke - 200LKR")
+        cmbFoodItems.Items.Add("Ice Cream - 300LKR")
+    End Sub
+End Class
